3de74e048c981cf89b60d9be57a3191443dc0e25
tangentdigitalagency <david@tangentdigitalagency.com>
2021-12-17 01:51:43 -0700

    bugsnag integration

--- src/index.js
+++ src/index.js
@@ -4,13 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router} from 'react-router-dom';
+import Bugsnag from '@bugsnag/js'
+import BugsnagPluginReact from '@bugsnag/plugin-react'
+
+Bugsnag.start({
+  apiKey: '85ebec6b5603348f8d8a6a2b1d2d2213',
+  plugins: [new BugsnagPluginReact()]
+})
 
 
+const ErrorBoundary = Bugsnag.getPlugin('react')
+  .createErrorBoundary(React)
+  
 ReactDOM.render(
+  <ErrorBoundary>
   <Router>
-  <App />
-</Router>,
-document.getElementById('root'),
+    <App />
+  </Router>
+  </ErrorBoundary>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
